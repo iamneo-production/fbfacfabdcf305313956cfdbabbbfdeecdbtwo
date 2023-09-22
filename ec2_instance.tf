b5fe9f9a458fe96bcefed06638234252d9049e61
Iamneo <techadmin@iamneo.ai>
2023-09-22 05:26:24 +0000

    Backup Commit

---
+++ ec2_instance.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-southeast-1"
+    access_key = "AKIAT4NKNOKFSXTBHCHY"
+    secret_key = "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"
+
+}
+
